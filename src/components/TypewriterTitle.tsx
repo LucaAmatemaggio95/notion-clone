7925cf995e094dc92de1cdc3ad6ad0070cb9f320
lamatemaggio <luca.amatemaggio@rcslab.it>
2023-11-20 12:52:51 +0100

    Created landing page & dashboard

---
+++ src/components/TypewriterTitle.tsx
@@ -0,0 +1,24 @@
+"use client";
+import Typewriter from "typewriter-effect";
+
+type Props = {};
+
+const TypewriterTitle = (props: Props) => {
+  return (
+    <Typewriter
+      options={{
+        loop: true,
+      }}
+      onInit={(typewriter) =>
+        typewriter
+          .typeString("🚀 Supercharged Productivity.")
+          .pauseFor(1000)
+          .deleteAll()
+          .typeString("🤖 AI enhanced")
+          .start()
+      }
+    />
+  );
+};
+
+export default TypewriterTitle;
