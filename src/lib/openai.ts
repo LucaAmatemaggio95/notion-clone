ecc1baea132f27e7a23df2f5c36f40d98bf0e3b0
lamatemaggio <luca.amatemaggio@rcslab.it>
2023-11-20 16:51:10 +0100

    Added OpenAI route

---
+++ src/lib/openai.ts
@@ -0,0 +1,36 @@
+import { Configuration, OpenAIApi } from "openai-edge";
+
+const config = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(config);
+
+export async function generateImagePrompt(name: string) {
+  try {
+    const response = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "system", // first role used by the system
+          content:
+            "You are a creative and helpful AI assistant capable of generating interesting thumbnail descriptions for my notes. Your output will be fetched into the DALLE API to generate a thumbnail. The description should be minimalistic and flat styled.",
+        },
+        {
+          role: "user", // user request
+          content: `Please geenrate a thumbnail description for my notebook titled ${name}`,
+        },
+      ],
+    });
+
+    const data = await response.json();
+
+    const imageDescription = data.choices[0].message.content;
+    return imageDescription as string;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+// export async function generateImage() {}
