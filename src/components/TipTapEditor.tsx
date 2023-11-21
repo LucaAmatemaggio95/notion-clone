7fcdd82099b44f261ba215a0325077406f8a12e9
lamatemaggio <luca.amatemaggio@rcslab.it>
2023-11-21 12:39:05 +0100

    Added text editor tip

--- src/components/TipTapEditor.tsx
+++ src/components/TipTapEditor.tsx
@@ -91,9 +91,18 @@ const TipTapEditor = ({ note }: Props) => {
           {saveNote.isPending ? "Saving..." : "Saved"}
         </Button>
       </div>
-      <div className="prose">
+      <div className="prose prose-sm w-full mt-4">
         <EditorContent editor={editor} />
       </div>
+
+      <div className="h-4"></div>
+      <span className="text-sm">
+        Tip: Press
+        <kbd className="mx-2 px-1 py-1.5 text-xs font-semibold bg-gray-100 border border-gray-200 rounded-lg">
+          Shift + A
+        </kbd>
+        for AI autocomplete
+      </span>
     </>
   );
 };
